019ef93bf64e2b8534959639c1c79dd5b9a9e55a
Sologala <dawsonwen123@gmail.com>
2023-03-16 21:45:20 +0800

    add threadpool

---
+++ cpp/threadpool/cmake/threadpoolConfig.cmake
@@ -0,0 +1,3 @@
+include(CMakeFindDependencyMacro)
+set(NAME threadpool)
+include("${CMAKE_CURRENT_LIST_DIR}/${NAME}Targets.cmake")
--- test/test_fmt.cpp
+++
@@ -1,5 +0,0 @@
-#include "pyp/fmt/fmt.hpp"
-int main() {
-    fmt::print("hahadhsjfhjk{1} {0} {2}", 123, 12312, "asdfadsf");
-    return 0;
-}
