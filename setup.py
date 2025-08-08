010b41e79cd96527d6cd55e230ce7f3fba48874c
WenDawson <dawsonwen123@gmail.com>
2025-08-08 23:47:59 +0800

    add uv

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
-from setuptools import setup, find_packages
+from skbuild import setup  # This line replaces 'from setuptools import setup'
 
 setup(
-    name='pyp',
-    version='0.0.1',
-    install_requires=[
-        'numpy',
-        'fire',
-        "imageio",
-        "pillow",
-        'importlib-metadata; python_version<"3.10"',
-    ],
-    packages=find_packages(
-    ),
-    entry_points={
-        'console_scripts': [
-            'pyp = pyp.entry:main_entry',
-        ]
-    }
+    name="pyp",
+    version="1.2.3",
+    description="a minimal example package (cpp version)",
+    author="The scikit-build team",
+    license="MIT",
+    packages=["hello"],
+    python_requires=">=3.9",
 )
+
+# setup(
+#     name='pyp',
+#     version='0.0.1',
+#     install_requires=[
+#         'numpy',
+#         'fire',
+#         "imageio",
+#         "pillow",
+#         'importlib-metadata; python_version<"3.10"',
+#     ],
+#     packages=find_packages(
+#     ),
+#     entry_points={
+#         'console_scripts': [
+#             'pyp = pyp.entry:main_entry',
+#         ]
+#     }
+# )
